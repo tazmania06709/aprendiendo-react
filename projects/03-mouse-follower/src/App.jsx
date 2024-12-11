a03f3ac9d87c4efeeb2e5ef890bd54c53d23075b
Josman Vicens Noa <tazmania06709>
2024-12-10 19:04:53 -0500

    update style

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -30,18 +30,32 @@ const FollowMouse = () => {
 
   }, [enabled])
 
+  // [] -> solo se ejecuta una vez cuando se monta el componente
+  // [enabled] -> se ejecuta cuando cambia enabled y cuando se monta el componente
+  // undefined -> se ejecuta cada vez que se renderiza el componente
+
+  // change body className
+  useEffect(() => {
+    document.body.classList.toggle('no-cursor', enabled)
+
+    return () => {
+      document.body.classList.remove('no-cursor')
+    }
+  }, [enabled])
+
   return (
     <>
       <div style={{
         position: 'absolute',
-        backgroundColor: '#09f',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
         borderRadius: '50%',
         opacity: 0.8,
         pointerEvents: 'none',
-        left: -20,
-        top: -20,
-        width: 40,
-        height: 40,
+        left: -25,
+        top: -25,
+        width: 50,
+        height: 50,
         transform: `translate(${position.x}px,${position.y}px)`,
       }} />
       <button onClick={() => setEnabled(!enabled)}>
