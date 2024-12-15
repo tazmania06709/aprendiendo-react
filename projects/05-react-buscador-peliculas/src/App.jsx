d07929724fae9a5842049daa6ee8597b7df99996
Josman Vicens Noa <tazmania06709>
2024-12-15 18:52:54 -0500

    add init 05

---
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div>
+      <form className='form'>
+        <input type="text" placeHolder='Avengers, Stars Wars, The Matrix,...' />
+        <button type='submit'>Buscar</button>
+      </form>
+      <h1>Prueba tecnica</h1>
+
+      <main>
+      //  No existen resultados.
+      </main>
+    </div>
+  )
+}
+
+export default App
